5f7a3667e83d4e4888364c0d934a8b7338718e5c
basiliage <>
2023-02-15 01:55:14 +0000

    commit message

--- README.md
+++ README.md
@@ -49,11 +49,28 @@ where $W(1) = 1$.
 - [ ] 4. (2 point) Now, derive the asymptotic behavior of $W(n)$ using $f(n) = 1$, $f(n) = \log n$ and $f(n) = n$. Then, generate actual values for $W(n)$ for your code and confirm that the trends match your derivations.
 
 **TODO: your answer goes here**
-
+f(n)=1 - W(n) = O(n)
+f(n)=log(n) - W(n) = O(nlogn)
+f(n)=n - W(n) = O(n^2)
+n = 10,20,30
+f(n) = 1,15,31
+f(n) = log(n)
+16.29
+35.58
+41.38
+f(n)=n
+36
+92
+128
 - [ ] 5. (4 points) Now that you have a nice way to empirically generate valuess of $W(n)$, we can look at the relationship between $a$, $b$, and $f(n)$. Suppose that $f(n) = n^c$. What is the asypmptotic behavior of $W(n)$ if $c < \log_b a$? What about $c > \log_b a$? And if they are equal? Modify `compare_work` to compare empirical values for different work functions (at several different values of $n$) to justify your answer. 
 
 **TODO: your answer goes here**
+no, they are not equal.
 
 - [ ] 6. (3 points) $W(n)$ is meant to represent the running time of some recursive algorithm. Suppose we always had $a$ processors available to us and we wanted to compute the span of the same algorithm. Implement the function `span_calc` to compute the empirical span, where the work of the algorithm is given by $W(n)$. Implement `test_compare_span` to create a new comparison function for comparing span functions. Derive the asymptotic expressions for the span of the recurrences you used in problem 4 above. Confirm that everything matches up as it should. 
 
 **TODO: your answer goes here**
+
+f(n) = 1, span = O(logn)
+f(n) = log(n), span = 0(logn^2)
+f(n) = n, span =(nlogn)
--- main.py
+++ main.py
@@ -5,62 +5,47 @@ CMPS 2200  Recitation 2
 ### the only imports needed are here
 import tabulate
 import time
+import math
 ###
 
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
-
+  if n<=1:
+    return n
+  else:
+    return a * simple_work_calc(n//b, a, b) + n
+  
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == 36
+	assert simple_work_calc(20, 3, 2) == 230
+	assert simple_work_calc(30, 4, 2) == 650
+assert simple_work_calc(20,3,3) == 56
+assert simple_work_calc(30,4,3) == 182
+assert simple_work_calc(40,5,3) == 330
 
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return a * work_calc(n//b, a, b, f) + f(n)
+    
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
 
 def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return span_calc(n//b, a, b, f) + f(n)
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
 
 def test_work():
 	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2,lambda n: 1) == 5
+	assert work_calc(20, 1, 2, lambda n: n*n) == 5
+	assert work_calc(30, 3, 2, lambda n: n) == 5
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
@@ -73,31 +58,70 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 	
 	"""
 	result = []
-	for n in input_sizes:
+	for n in sizes:
 		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
+		result.append((n,work_fn1(n),work_fn2(n)))
 	return result
 
 def print_results(results):
 	""" done """
-	print(tabulate.tabulate(results,
-							headers=['n', 'W_1', 'W_2'],
-							floatfmt=".3f",
-							tablefmt="github"))
+	print(tabulate.tabulate(results,headers=['n', 'W_1', 'W_2'],floatfmt=".3f",tablefmt="github"))
 
 def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
+  def work_fn1(n):
+    return work_calc(n, 2, 2, lambda n: n**(math.log(2, 2)- 1))
+  def work_fn2(n):
+    return work_calc(n, 2, 2, lambda n: n**(math.log(2,2)+1))
+  def work_fn3(n):
+    return work_calc(n,2,2, lambda n: n**(math.log(2,2)))
+  res = compare_work(work_fn1, work_fn2, work_fn3)
+  print_results(res)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
 
 def test_compare_span():
-	# TODO
+  def work_fn1(n):
+    return work_calc(n, 2, 2, lambda n: n**(math.log(2, 2)- 1))
+  def work_fn2(n):
+    return work_calc(n, 2, 2, lambda n: n**(math.log(2,2)+1))
+  def work_fn3(n):
+    return work_calc(n,2,2, lambda n: n**(math.log(2,2)))
+  res = compare_work(work_fn1, work_fn2, work_fn3)
+  print_results(res)
+
+print("Work:\n")
+print("f(n) = 1")
+print(work_calc(10, 2, 2, lambda n: 1))
+print(work_calc(20, 2, 2, lambda n: 1))
+print(work_calc(30, 2, 2, lambda n: 1))
+
+print("f(n) = log(n)")
+print(work_calc(10, 2, 2, lambda n: math.log(n)))
+print(work_calc(20, 2, 2, lambda n: math.log(n)))
+print(work_calc(30, 2, 2, lambda n: math.log(n)))
+
+print("f(n) = n")
+print(work_calc(10, 2, 2, lambda n: n))
+print(work_calc(20, 2, 2, lambda n: n))
+print(work_calc(30, 2, 2, lambda n: n))
+
+print("\nSpan:\n")
+print("f(n) = 1")
+print(span_calc(10, 2, 2, lambda n: 1))
+print(span_calc(20, 2, 2, lambda n: 1))
+print(span_calc(30, 2, 2, lambda n: 1))
+
+print("f(n) = log(n)")
+print(span_calc(10, 2, 2, lambda n: math.log(n)))
+print(span_calc(20, 2, 2, lambda n: math.log(n)))
+print(span_calc(30, 2, 2, lambda n: math.log(n)))
+
+print("f(n) = n")
+print(span_calc(10, 2, 2, lambda n: n))
+print(span_calc(20, 2, 2, lambda n: n))
+print(span_calc(30, 2, 2, lambda n: n))
+
+print("\nCompare Work")
+test_compare_work()
+
+print("\nCompareSpan")
+test_compare_span()
